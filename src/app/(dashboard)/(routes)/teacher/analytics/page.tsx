f6ba996720e18a0c3eff6fe79ab47644bef76178
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2023-12-17 17:45:58 +0530

    feat- Analytics page

--- src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,28 @@
-const AnalyticsPage = () => {
-    return ( <div>Analytics Page</div> );
-}
- 
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { getAnalytics } from "../../../../../../actions/getAnalytics";
+import DataCard from "./_components/dataCard";
+import Chart from "../../(root)/_components/chart";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Sales" value={totalSales} />
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+      </div>
+      <Chart data={data}/>
+    </div>
+  );
+};
+
 export default AnalyticsPage;
