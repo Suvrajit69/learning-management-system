49290d59a5bb83e635f25502b685ac378e281657
Suvrajit Mondal <suvrajitmondal359@gmail.com>
2023-12-20 17:08:20 +0530

    Footer section

--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -1,7 +1,6 @@
 import Logo from "./logo";
 import SidebarRoutes from "./sidebarRoutes";
 
-
 const Sidebar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
---
+++ src/components/footer.tsx
@@ -0,0 +1,29 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const Footer = () => {
+  return (
+    <footer className="w-full  bg-slate-200 p-5 md:pl-60">
+      <div className="flex flex-col md:flex-row  md:justify-between items-center">
+        <div className="flex flex-col items-center">
+          <span className="font-bold text-slate-700">About us</span>
+          <p className="font-medium text-sm text-slate-600">
+            Made and Loved❤️ from India🇮🇳{" "}
+          </p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Avatar className="h-28 w-28 mb-1">
+            <AvatarImage src="/Hero.jpg" />
+            <AvatarFallback>Our Founder</AvatarFallback>
+          </Avatar>
+          <h4 className="font-bold text-sm text-slate-700">Our Founder</h4>
+          <span className="font-normal text-base">Suvrajit Mondal</span>
+        </div>
+      </div>
+      <div className="text-center text-xs font-medium">
+        &#169;2024. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
